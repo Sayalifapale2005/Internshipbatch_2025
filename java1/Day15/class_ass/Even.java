18698d585c62354e1c8796e3186be949231a76af
Sayalifapale2005 <fapalesayali2005@gmail.com>
2025-06-23 16:30:35 +0530

    Day15

---
+++ java1/Day15/class_ass/Even.java
@@ -0,0 +1,16 @@
+package Day15;
+
+public class Even {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=20;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
---
+++ java1/Day15/class_ass/Number.java
@@ -0,0 +1,12 @@
+package Day15;
+
+public class Number {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+	      System.out.println(i);
+		}
+	}
+
+}
